7a4cd678da975cad98a8d71e43dd49d554302d29
Alberto Valverde <alberto@meteogrid.com>
2016-01-05 22:18:18 +0100

    clenaup event

--- src/Reflex/OpenLayers/Event.hs
+++ src/Reflex/OpenLayers/Event.hs
@@ -1,51 +1,32 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE JavaScriptFFI #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Reflex.OpenLayers.Event (on, on_) where
 
-import GHCJS.Marshal (ToJSVal(toJSVal), FromJSVal(fromJSVal))
-import GHCJS.Types
-import GHCJS.DOM.Types (toJSString)
+import GHCJS.Marshal.Pure (PToJSVal(pToJSVal), PFromJSVal(pFromJSVal))
 import GHCJS.Foreign.Callback (
-    Callback
-  , OnBlocked(ContinueAsync)
+    OnBlocked(ContinueAsync)
   , syncCallback1'
   , syncCallback1
   , releaseCallback
   )
+import GHCJS.Foreign.QQ
+import GHCJS.Types (JSVal, jsval)
 
-import Control.Monad ((>=>))
-
-data Event = Event
-
-instance FromJSVal Event where
-  fromJSVal _ = return (Just Event)
-
-on :: (ToJSVal a, ToJSVal b, FromJSVal e)
+on :: (PToJSVal a, PToJSVal b, PFromJSVal e)
    => String -> a -> (e -> IO b) -> IO (IO ())
 on eventName ob cb = do
-  jsOb <- toJSVal ob
-  jsCb <- syncCallback1' cb'
-  key <- js_on jsOb (toJSString eventName) jsCb
-  return (js_unByKey jsOb key >> releaseCallback jsCb)
-  where
-    cb' = (fromJSVal >=> maybe eErr cb >=> toJSVal)
-    eErr = fail "Reflex.OpenLayers.Event.on: could not convert event"
+  cb' <- syncCallback1' (fmap pToJSVal . cb . pFromJSVal)
+  let jsCb = jsval cb'
+  key :: JSVal <- [jsu|$r=`ob.on(`eventName, `jsCb);|]
+  return ([jsu_|`ob.unByKey(`key);|] >> releaseCallback cb')
 
-on_ :: (FromJSVal e, ToJSVal a)
+on_ :: (PFromJSVal e, PToJSVal a)
     => String -> a -> (e -> IO ()) -> IO (IO ())
 on_ eventName ob cb = do
-  jsOb <- toJSVal ob
-  jsCb <- syncCallback1 ContinueAsync cb'
-  key <- js_on jsOb (toJSString eventName) jsCb
-  return (js_unByKey jsOb key >> releaseCallback jsCb)
-  where
-    cb' = (fromJSVal >=> maybe eErr cb)
-    eErr = fail "Reflex.OpenLayers.Event.on_: could not convert event"
-
-foreign import javascript unsafe "$1.on($2, $3)"
-  js_on :: JSVal -> JSString -> Callback (JSVal -> IO a) -> IO JSVal
-
-foreign import javascript unsafe "$1.unByKey($2)"
-  js_unByKey :: JSVal -> JSVal -> IO ()
+  cb' <- syncCallback1 ContinueAsync (cb . pFromJSVal)
+  let jsCb = jsval cb'
+  key :: JSVal <- [jsu|$r=`ob.on(`eventName, `jsCb);|]
+  return ([jsu_|`ob.unByKey(`key);|] >> releaseCallback cb')
